f43a0b2476c30b5f1c28358db53aea36a215566d
RickardssonJ <rickardssonjimmy@gmail.com>
2025-04-09 11:19:44 +0200

    add axios

--- src/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  zapier: {
-    userCreatedWebhookUrl: process.env.ZAPIER_USER_CREATED_WEBHOOK_URL,
-    userUpdatedWebhookUrl: process.env.ZAPIER_USER_UPDATED_WEBHOOK_URL,
-  },
-}
